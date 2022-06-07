554395741ecb7cc38fe2c6ef29f13cfaef47b6b4
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-06-07 18:19:04 +0300

    finished assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// divide divides 2000 by 100
+const divide = x => 2000/100; 
+
+//has an arrow function called square"
+//square arrow function takes one parameter and multiplies it times itself"
+const square = x => x ** x;
+
+//has an arrow function called add"
+const add =(a,b) => a + b;
+
